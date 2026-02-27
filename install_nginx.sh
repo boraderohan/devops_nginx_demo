6b08de27cb0503b3cfa4e0078523fc54ccce9352
BoradeRohan <rohanborade17@gmai.com>
2026-02-27 19:13:15 +0000

    Chore: Minor changes in the nginx script

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# This is the script to install nginx and enable nginx
 
 sudo apt-get update -y 
 
 sudo apt install nginx -y
 
-sudo systemctl start nginx sudo systemctl enable nginx
+sudo systemctl start nginx 
+
+sudo systemctl enable nginx # This command is used to enable the nginx
 
 echo "NGINX Installed...."
